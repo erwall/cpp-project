c898684ee1a251680b758f511550fab063c97053
Fredric Berg <fredr1c@hotmail.com>
2016-03-30 11:24:32 +0200

    Added connections vector and modified registerConnection and deRegisterConnection.

--- inc/server.h
+++ inc/server.h
@@ -7,15 +7,19 @@
 #include <list>
 #include <utility>
 #include <memory>
-
+#include <vector>
 class Server
 {
 private:
 	std::list<std::pair<size_t, Newsgroup>> nglist;
 	int port;
 	bool ready;
+	
+	// Keeping track of connections to server
+	std::vector<std::shared_ptr<Connection>> connections;
 public:
-	Server(int);
+	explicit Server(int);
+	virtual ~Server();
 	bool isReady() const;
 	std::shared_ptr<Connection> waitForActivity() const;
 	void registerConnection(const std::shared_ptr<Connection>& conn);
--- src/app/server.cc
+++ src/app/server.cc
@@ -6,19 +6,40 @@ Server::Server(int port)
 	ready = true;
 }
 
-bool Server::isReady() const
-{
-	return ready;
+/* Removes all registered connections */
+
+Server::~Server() {
+	connections.clear();
 }
 
+/* Waits for activity on the port. Returns a previously registered
+connection object if an existing client wishes to communicate,
+nullptr if a new client wishes to communicate */
+
+std::shared_ptr<Connection> Server::waitForActivity() const {
+;
+}
+
+bool Server::isReady() const
+{
+	/* when is server not ready? */
+	return ready;
+}
+/* Registers a new connection */
 void Server::registerConnection(const std::shared_ptr<Connection>& conn)
 {
-	;
+	/* Check connection valid & stuff? */
+	if(!conn->isConnected()) {
+		connections.push_back(conn);
+	}
 }
 
 void Server::deregisterConnection(const std::shared_ptr<Connection>& conn)
 {
-	;
+	/* Removes conn from connections */
+	/* Unsure if works (fak eduroam) */
+	auto start = remove_if(connections.begin(), connections.end(), [&conn](Connection c) { return c == conn });
+	connections.erase(start,connections.end());
 }
 
 int main() {
