95651c098480be81e8cef9acd88fa0315c664767
Fredric Berg <fredr1c@hotmail.com>
2016-04-07 12:53:59 +0200

    Added MessageHandler to communicate between client and server

---
+++ src/lib/messagehandler.cc
@@ -0,0 +1,29 @@
+#include "messagehandler.h"
+using namespace std;
+int MessageHandler::readNumber() {
+	unsigned char byte1 = conn->read();	
+	unsigned char byte2 = conn->read();
+	unsigned char byte3 = conn->read();
+	unsigned char byte4 = conn->read();
+	return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+}
+void MessageHandler::writeNumber(int value) {
+	conn.write((value >> 24) & 0xFF);
+	conn.write((value >> 16) & 0xFF);
+	conn.write((value >> 8)	 & 0xFF);
+	conn.write(value & 0xFF);
+}
+void MessageHandler::writeString(const string& s) {
+	for (char c : s) {
+		conn->write(c);
+	}
+	conn->write('$');
+}
+string MessageHandler::readString() {
+	string s;
+	char ch;
+	while ((ch = conn.read()) != '$') {
+		s += ch;
+	}
+	return s;
+}
